7b0ce7e5256620a74e0a081efadeb5dcda31ded9
dilipsuthar60 <dilipsuthar212000@gmail.com>
2024-02-12 12:58:34 +0530

    flex box

--- App.js
+++ App.js
@@ -12,9 +12,22 @@ import Greet from "./components/Greet";
 const logoImage = require("./assets/adaptive-icon.png");
 export default function App() {
   return (
-    <View style={{ flex: 1, backgroundColor: "plum", paddingTop: 50 }}>
-      <Greet style={{backgroundColor:"red",margin: 10}} children={"box1"} />
-      <Greet style={{backgroundColor:"blue",margin: 10}} children={"box2"}/>
+    <View
+      style={{
+        backgroundColor: "plum",
+        paddingTop: 50,
+        borderWidth: 6,
+        borderStyle: "solid",
+        borderColor: "blue",
+        height:300,
+        marginTop:50
+      }}
+    >
+      <Greet style={{ backgroundColor: "red", margin: 10 }} children={"box1"} />
+      <Greet
+        style={{ backgroundColor: "blue", margin: 10 }}
+        children={"box2"}
+      />
     </View>
   );
 }
