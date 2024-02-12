3e24eda10a6fe8ee04bb7fcfd30beed3c90801a0
dilipsuthar60 <dilipsuthar212000@gmail.com>
2024-02-12 16:10:31 +0530

    different-different card

--- App.js
+++ App.js
@@ -11,22 +11,40 @@ export default function App() {
     moves: ["Scratch", "Ember", "Growl", "Leer"],
     weaknesses: ["Water", "Rock"],
   };
-  const charmanderData1 = {
-    name: "Charmander",
-    image: require("./assets/bulbasaur.png"),
-    type: "Fire",
-    hp: 39,
-    moves: ["Scratch", "Ember", "Growl", "Leer"],
-    weaknesses: ["Water", "Rock"],
+
+  const squirtleData = {
+    name: "Squirtle",
+    image: require("./assets/squirtle.png"), // Replace with the actual image path
+    type: "Water",
+    hp: 44,
+    moves: ["Tackle", "Water Gun", "Tail Whip", "Withdraw"],
+    weaknesses: ["Electric", "Grass"],
+  };
+
+  const bulbasaurData = {
+    name: "Bulbasaur",
+    image: require("./assets/bulbasaur.png"), // Replace with the actual image path
+    type: "Grass",
+    hp: 45,
+    moves: ["Tackle", "Vine Whip", "Growl", "Leech Seed"],
+    weaknesses: ["Fire", "Ice", "Flying", "Psychic"],
+  };
+
+  const pikachuData = {
+    name: "Pikachu",
+    image: require("./assets/pikachu.png"), // Replace with the actual image path
+    type: "Electric",
+    hp: 35,
+    moves: ["Quick Attack", "Thunderbolt", "Tail Whip", "Growl"],
+    weaknesses: ["Ground"],
   };
   return (
     <ScrollView>
     <SafeAreaView style={styles.container}>
       <Greet {...charmanderData}/>
-      <Greet {...charmanderData1}/>
-      <Greet {...charmanderData}/>
-      <Greet {...charmanderData}/>
-      <Greet {...charmanderData}/>
+      <Greet {...squirtleData}/>
+      <Greet {...pikachuData}/>
+      <Greet {...bulbasaurData}/>
     </SafeAreaView>
     </ScrollView>
   );
