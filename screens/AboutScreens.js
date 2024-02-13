3f767b0953f9f7b1e18faf3f4718bb80b9608d59
dilipsuthar60 <dilipsuthar212000@gmail.com>
2024-02-13 15:31:34 +0530

    initial setup for project

--- screens/AboutScreens.js
+++ screens/AboutScreens.js
@@ -1,27 +1,32 @@
-import { useNavigation } from '@react-navigation/native';
-import React, { Component } from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import { useNavigation } from "@react-navigation/native";
+import React, { Component } from "react";
+import { View, Text, StyleSheet, Button } from "react-native";
 
-const AboutScreens = ({route}) => {
-    const navigation =useNavigation()
-    const {name}=route.params
-    return (
-        <View style={styles.container}>
-            <Text style={{marginBottom:15}}>About us {name}</Text>
-            <Button title='back To Home Page' onPress={()=>{
-                navigation.navigate("Home")
-            }}/>
-        </View>
-    );
+const AboutScreens = ({ route }) => {
+  const navigation = useNavigation();
+  const { name } = route.params;
+  return (
+    <View style={styles.container}>
+      <Text style={{ marginBottom: 15 }}>
+        About us {name}
+      </Text>
+      <Button
+        title="back To Home Page"
+        onPress={() => {
+          navigation.navigate("Home");
+        }}
+      />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#2c3e50',
-    },
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#2c3e50"
+  }
 });
 
 export default AboutScreens;
--- screens/HomeScreens.js
+++ screens/HomeScreens.js
@@ -1,25 +1,27 @@
-import React, { Component } from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import React, { Component } from "react";
+import { View, Text, StyleSheet, Button } from "react-native";
 
-
-const HomeScreens = ({navigation}) => {
-    return (
-        <View style={styles.container}>
-            <Text style={{marginBottom:10}}>MyComponent</Text>
-            <Button title='About page' onPress={()=>{
-                navigation.navigate("About",{name:"dilip suthar pavta"})
-            }} />
-        </View>
-    );
+const HomeScreens = ({ navigation }) => {
+  return (
+    <View style={styles.container}>
+      <Text style={{ marginBottom: 10 }}>MyComponent</Text>
+      <Button
+        title="About page"
+        onPress={() => {
+          navigation.navigate("About", { name: "dilip suthar pavta" });
+        }}
+      />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#2c3e50',
-    },
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#2c3e50"
+  }
 });
 
 export default HomeScreens;
