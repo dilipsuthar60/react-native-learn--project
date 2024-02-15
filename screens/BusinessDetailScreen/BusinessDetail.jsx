1773d82a626167f4b406b19921b5eb82c20bfbf7
dilipsuthar60 <dilipsuthar212000@gmail.com>
2024-02-15 11:08:04 +0530

    business detail page

---
+++ screens/BusinessDetailScreen/BusinessDetail.jsx
@@ -0,0 +1,15 @@
+import { View, Text, StyleSheet } from 'react-native'
+import React from 'react'
+
+export default function BusinessDetail() {
+  return (
+    <View>
+      <Text>BusinessDetail</Text>
+    </View>
+  )
+}
+
+
+const styles = StyleSheet.create({
+    
+})
